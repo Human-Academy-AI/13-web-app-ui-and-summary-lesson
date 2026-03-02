f14fe27ea78106323af58effc1fc85e930ce0b5a
shimi@fuRo <furo.org>
2026-03-02 18:29:03 +0900

    新しい翻訳アプリのUIと仕様書を追加

---
+++ translate_ui_custom/static/main.js
@@ -0,0 +1,22 @@
+﻿function translateText() {
+  const inputText = document.getElementById("input-text").value;
+  const sourceLanguage = document.getElementById("source-language").value;
+  const targetLanguage = document.getElementById("target-language").value;
+
+  if (inputText !== "") {
+    axios
+      .get(
+        "/translate" +
+          "?input_text=" +
+          encodeURIComponent(inputText) +
+          "&source_lang=" +
+          sourceLanguage +
+          "&target_lang=" +
+          targetLanguage
+      )
+      .then((response) => {
+        const textbox = document.getElementById("output-text");
+        textbox.value = response.data;
+      });
+  }
+}
